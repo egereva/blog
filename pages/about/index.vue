c51ce8d3f683b09a469fbab106ca3b0146b747f5
Egereva Marina <marina.egereva@gmail.com>
2020-02-25 19:23:32 +0300

    nuxt.js модуль #1

---
+++ pages/about/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <section class="about">
+      <div class="container">
+        <h1 class="title">About my blog: </h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
+        <intro title="Lorem ipsum dolor sit amet"/>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
+        <img src="@/assets/img/designer.jpeg" alt="">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+        </p>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  import intro from '@/components/UI/Intro.vue'
+
+  export default {
+    components: {intro}
+  }
+</script>
+
+<style lang="scss">
+  .about {
+    text-align: center;
+
+    p {
+      margin-bottom: 10px;
+    }
+
+  }
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="wrapper-content wrapper-content--fixed">
+    <promo/>
+    <intro title = "My lasts posts:" />
+    <postsList :posts="posts"/>
+  </div>
+</template>
+
+<script>
+import promo from '@/components/Promo.vue'
+import intro from '@/components/UI/Intro.vue'
+import postsList from '@/components/blog/PostsList.vue'
+
+export default {
+  components: {
+    promo,
+    intro,
+    postsList
+  },
+  data () {
+    return {
+      posts: [
+        {
+          id: 1,
+          title: '1 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut',
+          img: 'https://lawnuk.com/wp-content/uploads/2016/08/sprogs-dogs.jpg'
+        },
+        {
+          id: 2,
+          title: '2 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut',
+          img: 'https://iheartdogs.com/wp-content/uploads/2017/09/Brite-Bite-0021-2.jpg'
+        },
+        {
+          id: 3,
+          title: '3 post',
+          descr: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut',
+          img: 'https://avatars.mds.yandex.net/get-pdb/964669/905e9ab5-f170-42a8-beea-ed37400c3883/s1200'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
